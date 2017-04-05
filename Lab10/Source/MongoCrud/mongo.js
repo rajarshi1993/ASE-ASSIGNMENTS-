48f4c6d3617326e4a4fd83692b77beaa046f44e7
rajarshi1993 <rajarshitarafdar3193@gmail.com>
2017-04-04 21:36:47 -0500

    Commit

---
+++ Lab10/Source/MongoCrud/mongo.js
@@ -0,0 +1,123 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var bodyParser = require("body-parser");
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var url = 'mongodb://admin:admin@ds147510.mlab.com:47510/raj';
+var ObjectID = require('mongodb').ObjectID;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/create', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        insertDocument(db, req.body, function() {
+            res.write("Successfully inserted");
+            res.end();
+        });
+    });
+})
+
+app.get('/get', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+
+        db.collection('raj').find().toArray(function(err, result){
+            if(err)
+            {
+                res.write("get Failed");
+                res.end();
+            }else
+            {
+
+                res.send(JSON.stringify(result));
+            }
+            console.log("Got All Documents");
+
+        });
+    });
+
+})
+
+app.get('/update/:s_id', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        var find={};
+        var newData={};
+
+        if(req.params.s_id){
+            find._id = new ObjectID(req.params.s_id);
+        }
+        if(req.query.ISBN){
+            newData.ISBN = req.query.ISBN;
+        }
+        if(req.query.bookName){
+            newData.bookName = req.query.bookName;
+        }
+
+        if(req.query.authorName){
+            newData.authorName = req.query.authorName;
+        }
+
+        db.collection('raj').update(find,{'$set':newData},
+            function(err,result){
+                if(err)
+                    throw err;
+                else
+                    res.send("Update success !");
+            });
+    });
+})
+
+app.get('/delete/:s_id', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        var find={};
+
+        if(req.params.s_id)
+            find._id = new ObjectID(req.params.s_id);
+
+        db.collection('raj').remove(find,function(err,result){
+            if(err)
+                throw err;
+            res.send('Delete Success');
+        });
+    });
+})
+
+var insertDocument = function(db, data, callback) {
+    db.collection('raj').insertOne( data, function(err, result) {
+        if(err)
+        {
+            res.write("Registration Failed, Error While Registering");
+            res.end();
+        }
+        console.log("Inserted a document into the books collection.");
+        callback();
+    });
+};
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
